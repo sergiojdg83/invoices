b12b09ef3bb47ed6770598116ff3f17fc2b376b1
sergiojdg83 <sergiojdg83@gmail.com>
2022-03-11 17:26:57 -0500

    Main View Controller

--- webapp/controller/mainView.controller.js
+++ webapp/controller/mainView.controller.js
@@ -1,14 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
+/**
+ * @param {typeof sap.ui.core.mvc.Controller} Controller
+ * @param {typeof sap.ui.model.Filter} Filter
+ * @param {typeof sap.ui.model.FilterOperator} FilterOperator
+ * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+ */
+
     function (Controller) {
         "use strict";
 
         return Controller.extend("logaligroup.invoices.controller.mainView", {
             onInit: function () {
+
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("./model/selectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen");
 
             }
         });
