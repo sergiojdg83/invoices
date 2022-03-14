83dd55f784f95ce57a2c8c67a8cea8d01d84992e
sergiojdg83 <sergiojdg83@gmail.com>
2022-03-14 16:28:40 -0400

    Select List

--- webapp/controller/mainView.controller.js
+++ webapp/controller/mainView.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
 ],
 /**
  * @param {typeof sap.ui.core.mvc.Controller} Controller
