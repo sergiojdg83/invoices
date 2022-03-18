609a2bf373b3831e962cde585dd7f06d75b8ae26
sergiojdg83 <sergiojdg83@gmail.com>
2022-03-18 01:35:10 -0400

    AppEnd

--- webapp/controller/mainView.controller.js
+++ webapp/controller/mainView.controller.js
@@ -1,17 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator",
-    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/FilterOperator"
 ],
-/**
- * @param {typeof sap.ui.core.mvc.Controller} Controller
- * @param {typeof sap.ui.model.Filter} Filter
- * @param {typeof sap.ui.model.FilterOperator} FilterOperator
- * @param {typeof sap.ui.model.json.JSONModel} JSONModel
- */
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.Filter} _Filter
+     * @param {typeof sap.ui.model.FilterOperator} _FilterOperator
+     */
 
-    function (Controller) {
+    function (Controller, _Filter, _FilterOperator) {
         "use strict";
 
         return Controller.extend("logaligroup.invoices.controller.mainView", {
@@ -22,6 +20,32 @@ sap.ui.define([
                 oJSONModel.loadData("./model/selectionScreenMenu.json");
                 oView.setModel(oJSONModel, "selectionScreen");
 
+                const oJSONModel1 = new sap.ui.model.json.JSONModel();
+                const oView1 = this.getView();
+                oJSONModel1.loadData("./model/invoicesList.json");
+                oView1.setModel(oJSONModel1, "invoicesList");
+            },
+            onFilter: function (_oEvent){
+               const oData = this.getView().getModel("selectionScreen").getData();
+               let filters = [];
+               if(oData.ShipName !== "") {
+                 filters.push(new _Filter ("ShipName", _FilterOperator.Contains, oData.ShipName));
+               }
+               if(oData.CountryKey !== "") {
+                filters.push(new _Filter ("Country", _FilterOperator.EQ, oData.CountryKey));
+              }
+              const oList = this.getView().byId("invoicesList");
+              const oBinding = oList.getBinding("items");
+              oBinding.filter(filters);
+            },
+            onClearFilter: function (){
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName", "");
+                oModelSelScreen.setProperty("/CountryKey", "");
+
+                const oList = this.getView().byId("invoicesList");
+                const oBinding = oList.getBinding("items");
+                oBinding.filter([]);
             }
         });
     });
